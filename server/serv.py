75a7e48ce84cd3178b19fe831844d7c6565cad1f
Gabriel Blanco <blancgab@gmail.com>
2015-04-14 15:49:09 -0400

    learn how to stderr, Brian :)

--- server/serv.py
+++ server/serv.py
@@ -2,29 +2,49 @@ import socket
 import sys
 import csv
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# In case two few/many arguments
+def usage():
+    print """
+    Usage:
+        python serv.py [csv_file]
 
-# Bind the socket to the address given on the command line
-server_address = ('', 5554)
-sock.bind(server_address)
-print (sys.stderr, 'starting up on %s port %s' % sock.getsockname())
-sock.listen(1)
+    """
 
+# Sets up and runs the code
+if __name__ == '__main__':
 
-while True:
-    print(sys.stderr, 'waiting for a connection')
-    f= open(sys.argv[1], 'rt')
-    connection, client_address = sock.accept()
-    try:
-        print(sys.stderr, 'client connected:', client_address)
-        reader = csv.DictReader(f)
-        for row in reader:
-            data = str(row) +'\n'
-            if data:
-                connection.sendall(data.encode())
-            else:
-                break
-    finally:
-        f.close()
-        connection.close()
+    if len(sys.argv)!=2:
+        usage()
+        sys.exit(2)
+
+    # Create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Bind the socket to the address given on the command line
+    server_address = ('', 5554)
+    sock.bind(server_address)
+    sys.stderr.write('starting up on port {}\n'.format(sock.getsockname()))
+
+    sock.listen(1)
+
+    while True:
+        sys.stderr.write('Waiting for a connection...\n')
+        f = open(sys.argv[1], 'rt')
+        try:
+            connection, client_address = sock.accept()
+        except:
+            sys.stderr.write('\nServer halted while listening\n')            
+            sys.exit(2)
+
+        try:
+            sys.stderr.write('Client [{}] has connected\n'.format(client_address))
+            reader = csv.DictReader(f)
+            for row in reader:
+                data = str(row) +'\n'
+                if data:
+                    connection.sendall(data.encode())
+                else:
+                    break
+        finally:
+            f.close()
+            connection.close()
